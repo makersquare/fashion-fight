030ece5188d7ddba2df59638caa71002b32f775b
Gilbert <gilbertbgarza@gmail.com>
2013-10-16 14:00:38 -0500

    Better collection filtering

--- app/assets/javascripts/app/models/comment.js
+++ app/assets/javascripts/app/models/comment.js
@@ -3,7 +3,12 @@
   window.Comment = Backbone.Model.extend({});
 
   window.CommentWall = Backbone.Collection.extend({
-    model: Comment
+    model: Comment,
+    filterBySide: function (side) {
+      return this.filter(function (comment) {
+        return comment.get('side') === side;
+      });
+    }
   });
 
 })();
--- app/assets/javascripts/app/views/vote-count-view.js
+++ app/assets/javascripts/app/views/vote-count-view.js
@@ -9,15 +9,8 @@
       this.listenTo(this.collection, 'add remove', this.render);
     },
     render: function () {
-      // Store a reference to `this.side` so we can use it via a closure
-      var side = this.side;
-
       // Gets a new array of comment models that voted for our side
-      var comments = this.collection.filter(function (comment) {
-        // Uses `side` instead of `this.side`
-        // Why? The value of `this` here is not the same as outside this function.
-        return comment.get('side') === side;
-      });
+      var comments = this.collection.filterBySide(this.side);
 
       // We don't have a model, but we do have our vote count.
       // Updates the vote count via our template.
