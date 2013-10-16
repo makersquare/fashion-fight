45a69429c1a09800217f0872a07ce5af7fc0774d
Gilbert <gilbertbgarza@gmail.com>
2013-10-16 15:29:32 -0500

    Comments and whitespace

--- app/assets/javascripts/app/models/comment.js
+++ app/assets/javascripts/app/models/comment.js
@@ -4,7 +4,9 @@
 
   window.CommentWall = Backbone.Collection.extend({
     model: Comment,
+
     filterBySide: function (side) {
+      // Gets all comments that voted for the given side.
       return this.filter(function (comment) {
         return comment.get('side') === side;
       });
